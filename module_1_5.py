9729b18c5e38b4fa89ec05dc494abc26d905ae9f
Zerber7 <zerber_@mail.ru>
2024-09-22 20:15:25 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,7 @@
+immutable_var = (333, 777, 'АРШИН', 'САЖЕНЬ', 'УЙМА')
+print(immutable_var)
+mutable_list = [333, 777777, 'АРШИН', 'САЖЕНЬ', 'УЙМА']
+print(mutable_list)
+mutable_list.extend(['ПЯДЬ', 8888888])
+print(mutable_list)
+
